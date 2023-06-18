2756859dafd8093c33754e50e014d938ea35b441
Nyore <aamonthedon@gmail.com>
2023-06-19 00:11:39 +0100

    Handle Errors

--- src/components/pages/routes.tsx
+++ src/components/pages/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import HomePage from "./HomePage";
 import GameDetailPage from "./GameDetailPage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
